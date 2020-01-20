9ddda09d271a913dbd425a97bbe9e58cd5ad37fd
Tom Frenzel <admin@tom-frenzel.de>
2020-01-20 14:23:23 +0100

    Update Tests + CI

--- tests/dotnet-structuring.tests/ExecuteTest.cs
+++ tests/dotnet-structuring.tests/ExecuteTest.cs
@@ -1,14 +1,70 @@
+using dotnet_structuring.library;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace dotnet_structuring.tests
 {
+
     [TestClass]
     public class ExecuteTest
     {
-        [TestMethod]
-        public void CreateScriptTest()
+        Variables _ = new Variables();
+        int LogNum;
+        string CurrentLog;
+        private void WireEventHandlers(Execute e)
         {
-            var Directories = new library.Variables.Directories()
+            ExecutionHandler handler = new ExecutionHandler(OnIncommingEventLog);
+            e.LogEvent += handler;
         }
+
+        public void OnIncommingEventLog(object sender, EventLogger e)
+        {
+            CurrentLog = e.logs;
+        }
+
+        [TestMethod]
+        public void StandardWinTest()
+        {
+            _.ProjectName = "TestProject";
+            _.Directory = Directory.GetCurrentDirectory() + @"\temp";
+            _.Artifacts = "artifacts";
+            _.Build = "build";
+            _.Docs = "docs";
+            _.Lib = "lib";
+            _.Samples = "samples";
+            _.Packages = "packages";
+            _.Test = "test";
+            _.ProjectType = "New Project";
+            _.SelectedTemplate = "console";
+            _.Options = "";
+            _.NETCommand = " new " + _.SelectedTemplate + " " + _.Options + "-o src/" + _.ProjectName + " -n " + _.ProjectName;
+            _.Directories = new string[]
+            {
+             _.Directory,
+             _.Artifacts,
+             _.Build,
+             _.Docs,
+             _.Lib,
+             _.Samples,
+             _.Packages,
+             _.Test,
+            };
+
+            Directory.CreateDirectory("temp");
+            
+
+            Execute OutputLogs = new Execute();
+            WireEventHandlers(OutputLogs);
+            OutputLogs.CreateScript(_.Directories, _.NETCommand, _.ProjectName);
+
+            Assert.AreEqual("Done.", CurrentLog);
+            Directory.Delete("temp", true);
+        }
+
+
     }
 }
+
