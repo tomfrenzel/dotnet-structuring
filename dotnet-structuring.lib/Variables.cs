30d520d2ce8a417d848a849df7633f5df26a8085
Tom Frenzel <admin@tom-frenzel.de>
2020-01-20 11:59:19 +0100

    Adding Tests

---
+++ dotnet-structuring.lib/Variables.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace dotnet_structuring.library
+{
+    public class Variables
+    {
+        public string SelectedTemplate
+        {
+            get;
+            set;
+        }
+        public string ProjectType
+        {
+            get;
+            set;
+        }
+        public string Directory
+        {
+            get;
+            set;
+        }
+        public string Artifacts
+        {
+            get;
+            set;
+        }
+        public string Build
+        {
+            get;
+            set;
+        }
+        public string Docs
+        {
+            get;
+            set;
+        }
+        public string Lib
+        {
+            get;
+            set;
+        }
+        public string Packages
+        {
+            get;
+            set;
+        }
+        public string Samples
+        {
+            get;
+            set;
+        }
+        public string Test
+        {
+            get;
+            set;
+        }
+        public string Options
+        {
+            get;
+            set;
+        }
+        public string ProjectName
+        {
+            get;
+            set;
+        }
+        public string NETCommand
+        {
+            get;
+            set;
+        }
+        public string[] Directories
+        {
+            get;
+            set;
+        }
+        public int ProccessAmount
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ tests/dotnet-structuring.tests/ExecuteTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dotnet_structuring.tests
+{
+    [TestClass]
+    public class ExecuteTest
+    {
+        [TestMethod]
+        public void CreateScriptTest()
+        {
+            var Directories = new library.Variables.Directories()
+        }
+    }
+}
