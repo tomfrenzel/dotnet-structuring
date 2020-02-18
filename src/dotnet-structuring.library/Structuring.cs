eebf97f5bdb6f101e5a5b31009cfae319cec5d08
Tom <tmfr@mms-t-systems.de>
2020-02-18 12:07:59 +0000

    Update Process

--- src/dotnet-structuring.library/Structuring.cs
+++ src/dotnet-structuring.library/Structuring.cs
@@ -5,33 +5,26 @@ using System.IO;
 using System.Threading.Tasks;
 using static dotnet_structuring.library.StructuringDelegate;
 
-namespace dotnet_structuring.library
-{
-    public class Structuring
-    {
+namespace dotnet_structuring.library {
+    public class Structuring {
         public event StructuringHandler LogEvent;
 
-        private void WriteLog(string logs)
-        {
-            EventLogger log = new EventLogger
-            {
+        private void WriteLog (string logs) {
+            EventLogger log = new EventLogger {
                 Logs = logs
             };
-            LogEvent.Invoke(this, log);
+            LogEvent.Invoke (this, log);
         }
 
-        public async Task RunStructuringAsync(string Output, IEnumerable<string> Directories, string NETCommand, string ProjectName)
-        {
-            DirectoryInfo OutputDirectory = new DirectoryInfo(Output + @"\" + ProjectName);
+        public async Task RunStructuringAsync (string Output, IEnumerable<string> Directories, string NETCommand, string ProjectName) {
+            DirectoryInfo OutputDirectory = new DirectoryInfo (Output + @"\" + ProjectName);
 
-            if (!OutputDirectory.Exists)
-            {
-                Directory.CreateDirectory(OutputDirectory.FullName);
-                CreateDirectories(Directories, OutputDirectory);
+            if (!OutputDirectory.Exists) {
+                Directory.CreateDirectory (OutputDirectory.FullName);
+                CreateDirectories (Directories, OutputDirectory);
 
-                await Task.Factory.StartNew(() =>
-                {
-                    Process p = new Process();
+                await Task.Factory.StartNew (() => {
+                    Process p = new Process ();
 
                     p.StartInfo.WorkingDirectory = OutputDirectory.FullName;
                     p.StartInfo.FileName = "dotnet";
@@ -39,45 +32,35 @@ namespace dotnet_structuring.library
                     p.StartInfo.UseShellExecute = false;
                     p.StartInfo.RedirectStandardOutput = true;
                     p.StartInfo.CreateNoWindow = true;
-                    p.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
-                    {
+                    p.OutputDataReceived += new DataReceivedEventHandler ((sender, e) => {
                         // Prepend line numbers to each line of the output.
-                        WriteLog(e.Data);
+                        WriteLog (e.Data);
                     });
-                    p.BeginOutputReadLine();
+
                     p.EnableRaisingEvents = true;
-                    p.Exited += (sender, e) =>
-                    {
-                        WriteLog("Done.");
-                        p.Kill();
+                    p.Exited += (sender, e) => {
+                        WriteLog ("Done.");
+                        p.Kill ();
                     };
-                    p.Start();
-                    
-                    p.WaitForExit();
+                    p.Start ();
+                    p.BeginOutputReadLine ();
+                    p.WaitForExit ();
                 });
-            }
-            else
-            {
-                WriteLog("A Project with this Name already exists!");
+            } else {
+                WriteLog ("A Project with this Name already exists!");
             }
         }
 
-        private void CreateDirectories(IEnumerable<string> Directories, DirectoryInfo OutputDirectory)
-        {
-            foreach (string element in Directories)
-            {
-                if (element != null)
-                {
+        private void CreateDirectories (IEnumerable<string> Directories, DirectoryInfo OutputDirectory) {
+            foreach (string element in Directories) {
+                if (element != null) {
                     var DirectoryBeingCreated = OutputDirectory + @"\" + element;
 
-                    if (Directory.Exists(DirectoryBeingCreated))
-                    {
-                        WriteLog("Directory " + DirectoryBeingCreated + " already exists");
-                    }
-                    else
-                    {
-                        var result = Directory.CreateDirectory(DirectoryBeingCreated);
-                        WriteLog("Directory " + result.FullName + " successfully created!");
+                    if (Directory.Exists (DirectoryBeingCreated)) {
+                        WriteLog ("Directory " + DirectoryBeingCreated + " already exists");
+                    } else {
+                        var result = Directory.CreateDirectory (DirectoryBeingCreated);
+                        WriteLog ("Directory " + result.FullName + " successfully created!");
                     }
                 }
             }
